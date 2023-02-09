4712e8de56428201f762a1e2cdea1025b635a493
Vishwa <110300383+vishwa2204@users.noreply.github.com>
2023-02-09 19:59:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Order Placed with ALERT Box Function 
+
+function clickEvt() {
+
+    alert("Order Placed");
+    console.log('Order Placed');
+};
+
+function searchEvt() {
+    alert("Waiting For Connection Loading...")
+};
+
+const headerFun = document.querySelector(".img");
+
+headerFun.addEventListener('click', () => {
+    alert("Hello World")
+})
+
